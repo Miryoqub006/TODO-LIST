1f04f950f80fac52c4ef269913733026255c9ea7
Miryoqub006 <158826997+Miryoqub006@users.noreply.github.com>
2026-06-02 16:40:50 +0500

    DbContext Added

--- ToDoList/src/ToDoList.Api/Data/AppDbContext.cs
+++ ToDoList/src/ToDoList.Api/Data/AppDbContext.cs
@@ -1,5 +1,9 @@
-﻿namespace ToDoList.Api.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Api.Entities;
 
-public class AppDbContext
+namespace ToDoList.Api.Data;
+
+public class AppDbContext : DbContext
 {
+    public DbSet<ToDoItem> ToDoItems { get; set; }
 }
