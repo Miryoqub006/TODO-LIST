9ce20f93f7574c7f15aa2a508b389249fbf82be9
Miryoqub006 <158826997+Miryoqub006@users.noreply.github.com>
2026-06-02 16:53:21 +0500

    Migration is done

--- ToDoList/src/ToDoList.Api/Data/AppDbContext.cs
+++ ToDoList/src/ToDoList.Api/Data/AppDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ToDoList.Api.Entities;
 
 namespace ToDoList.Api.Data;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
     public DbSet<ToDoItem> ToDoItems { get; set; }
 }
